b51299c26fd6ddf4a743de0fa2ad24d3a949a520
tatiana perez <tjperez@ncsu.edu>
2021-01-29 13:59:39 -0500

    step 2, first js additions

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log('Add validation!');
+
+
+window.addEventListener.EventListener('submit', event => { 
+event.preventDefault()
+})
+
+form.addEventListener('click', validate)
+
+function validate(event) {
+
+
+}
